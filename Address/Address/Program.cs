fe91fcd0360ae75ac9f8c00e30571a4d010a07dd
Stanislav <S.Zelyunko_FIT_2_21_B_d@knute.edu.ua>
2022-09-13 18:14:41 +0300

    Add Address

---
+++ Address/Address/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Address
+{
+    internal class Address
+    {
+        public string index;
+        public string country;
+        public string city;
+        public string street;
+        public string house;
+        public string apartment;
+
+        public void Print()
+        {
+            Console.WriteLine($"Index: {index};");
+            Console.WriteLine($"Country: {country};");
+            Console.WriteLine($"City: {city};");
+            Console.WriteLine($"Street: {street};");
+            Console.WriteLine($"House: {house};");
+            Console.WriteLine($"Apartment: {apartment}.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address();
+            address.index = "02000";
+            address.country = "Ukraine";
+            address.city = "Kyiv";
+            address.street = "Lobachevsky";
+            address.house = "23";
+            address.apartment = "510";
+            address.Print();
+        }
+    }
+}
