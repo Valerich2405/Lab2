69fbe6681b3dbba4a67ffa024e6ce2a91f9f4ba5
Stanislav <S.Zelyunko_FIT_2_21_B_d@knute.edu.ua>
2022-09-16 00:58:22 +0300

    Add User

---
+++ User/User/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace User
+{
+    class User
+    {
+        public string Login;
+        public string Name;
+        public string Surname;
+        public int Age;
+        public string Date;
+
+        public void Information()
+        {
+            Console.WriteLine("Enter your login:");
+            Login = Console.ReadLine();
+
+            Console.WriteLine("Enter your name:");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Enter your surname:");
+            Surname = Console.ReadLine();
+
+            Console.WriteLine("Enter your age:");
+            Age = Convert.ToInt32(Console.ReadLine());
+
+            Date = DateTime.Now.ToString();
+
+            Console.WriteLine();
+            Console.WriteLine($"Your login: {Login};");
+            Console.WriteLine($"Your name: {Name} {Surname};");
+            Console.WriteLine($"Your age: {Age};");
+            Console.WriteLine($"Date of filling out the questionnaire: {Date}.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.Information();
+        }
+    }
+}
